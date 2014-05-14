35b853255aad018149be9de68aa09ac1aaa00a9d
dunnk2 <kjdunn02@gmail.com>
2014-05-14 01:10:21 -0500

    Began adding multi-quiz functionality

--- Quiz App/Script.js
+++ Quiz App/Script.js
@@ -1,33 +1,25 @@
 "use strict"
 
 /* Features to add:
-finished- set correct answers as a property 
-- show answers visually at end of quiz, with 5 small pageviews and the users answer marked in red and the correct answer marked in green.
-finished- add css transitions. 
-finished - make everything centered
-finished - add progress bar
-- make a bunch of different quizzes and have them choose
-finished - change the mouse to a pointer when the user hovers over questions or answers
-
+- add create question functionality
+- add multiple quiz functionality
+- change the gif results to being percentage based instead of counter based. 
 */
 
 
-//global variables
+//global variables. I have two counters and four arrays.
 
 var questionCounter = 0;
 var correctCounter = 0;
-
-//var theQuestions = ["What color is the sky at noon?", "In what city is the Bastille located?", "What is the most beautiful state?", "What Holiday is celebrated on July 4?", "How do you complete a quiz app?"];
+var createQuestionCounter = 0;
+var numOfQuiz = 0;
 var questions = [];
 var selectedAnswers = [];
 var correctAnswers = [];
+var userCreatedQuestions = [];
+var quizzes = [];
 
-var protoQuestion = {
-    author: "Kyle",
-    changeQuestion: function () {
-        this["question"] = "testing the change";
-    }
-};
+
 
 var QuestionConstructor = function (questionText, A, B, C, D, correctAnswer) {
     var question = questionText;
@@ -58,13 +50,17 @@ var QuestionConstructor = function (questionText, A, B, C, D, correctAnswer) {
 
 };
 
-QuestionConstructor["prototype"] = protoQuestion;
-QuestionConstructor["prototype"]["changeQuestion"] = function (newQuestionText) {
-    this.question = newQuestionText;
+//creates prototype object, statement beneath object assigns it as QuestionConstructors prototype
+var protoQuestion = {
+    author: "Kyle",
+    changeQuestion: function () {
+        this["question"] = "testing the change";
+    }
 };
+QuestionConstructor["prototype"] = protoQuestion;
 
 
-
+// creating our questions using the question constructor
 var q1 = new QuestionConstructor("What color is the sky?", "red", "blue", "green", "yellow", 2);
 var q2 = new QuestionConstructor("Where is the Bastille located?", "London", "Moscow", "Seoul", "Paris", 4);
 var q3 = new QuestionConstructor("What is the most beautiful state?", "New Jersey", "Virginia", "Washington", "Colorado", 3);
@@ -72,20 +68,38 @@ var q4 = new QuestionConstructor("What Holiday is celebrated July 4?", "Independ
 var q5 = new QuestionConstructor("How do you complete a quiz app?", "With great ease", "By using stack overflow", "Through pain and suffering", "By playing flappy bird", 3);
 var q6 = new QuestionConstructor("What's the best football team?", "Oregon", "San Francisco", "Seattle Seahawks", "Atlanta Falcons", 3);
 
-
 //add all the questions to an array
 questions.push(q1, q2, q3, q4, q5, q6);
+
+quizzes.push(questions);
+
+//check for local store\age
+/*
+if (localStorage) {
+    localStorage.setItem("q1", new QuestionConstructor("What color is the sky?", "red", "blue", "green", "yellow", 2));
+    localStorage.setItem("q2", new QuestionConstructor("Where is the Bastille located?", "London", "Moscow", "Seoul", "Paris", 4));
+    localStorage.setItem("q3", new QuestionConstructor("What is the most beautiful state?", "New Jersey", "Virginia", "Washington", "Colorado", 3));
+    localStorage.setItem("q4", new QuestionConstructor("What Holiday is celebrated July 4?", "Independence Day", "Halloween", "Easter", "Thanksgiving", 1));
+    localStorage.setItem("q5", new QuestionConstructor("How do you complete a quiz app?", "With great ease", "By using stack overflow", "Through pain and suffering", "By playing flappy bird", 3));
+    localStorage.setItem("q6", new QuestionConstructor("What's the best football team?", "Oregon", "San Francisco", "Seattle Seahawks", "Atlanta Falcons", 3));
+} else {
+    alert("You're browser is not supported!")
+}
+*/
 
 //content wrapper begins hidden and is opened in the first function
 document.getElementById("contentWrapper").setAttribute("class", "hide");
 
 //function that changes the question if called
-questions[0].changeQuestion("test");
+//questions[0].changeQuestion("test");
 
 //function that is called when the start button is clicked.
-var first = function () {
-    //hides the begin quiz button
-    var beginQuiz = document.getElementById("start");
+var first = function (path) {
+    //hides the take quiz button
+    var beginQuiz = document.getElementById("takeQuiz");
+    beginQuiz.setAttribute("class", "hide");
+
+    var beginQuiz = document.getElementById("createQuiz");
     beginQuiz.setAttribute("class", "hide");
 
     //unhide the contentwrapper
@@ -95,27 +109,84 @@ var first = function () {
     document.getElementById("question").setAttribute("class", "");
 
     //calls the question function that will display the answers
-    displayQuestion();
+    //need to change the take path condition here to execute a display quizzes option,
+    //if (path === "take") {
+    //    displayQuestion();
+    //} else if (path === "create") {
+    //    createQuestion();
+    //}
+    if (path === "take") {
+        displayQuizzes();
+    } else if (path === "create") {
+        createQuestion();
+    }
 };
 
+//displays quizzes
+var displayQuizzes = function () {
+    for (var i = 0; i < quizzes.length; i++) {
+        //How do I get displayquestion to operate on the right quiz every time? I could try using a global variable. Passing something to displayquestion won't work, since 
+        document.getElementById("question").innerHTML += "<div class='quizzesDisplayed' onclick='displayQuestion(" + i + ");'>Quiz " + i + " </div>";
+    }
+};
 
+//function that displays input fields to create questions
+var createQuestion = function () {
+    //var createQuestionCounter = 0;
+    document.getElementById("question").innerHTML = "";
+    document.getElementById("question").innerHTML +=
+        "<div id='createQuestionHead'>Create Question " + (createQuestionCounter + 1) + "</div>"
+        + "<br />" + "<p class='creators' id='newQuestion'> Question: " + "<textarea name='newQ' cols='20' rows='4'/>Enter your question</textarea>" + "</p>"
+        + "<br />" + "<p class='creators' id='userAns1'> Answer 1: " + "<input type='text' name='ans1' size='15' maxlength='30' />" + "</p>"
+        + "<br />" + "<p class='creators' id='userAns2'> Answer 2: " + "<input type='text' name='ans2' size='15' maxlength='30' />" + "</p>"
+        + "<br />" + "<p class='creators' id='userAns3'> Answer 3: " + "<input type='text' name='ans3' size='15' maxlength='30' />" + "</p>"
+        + "<br />" + "<p class='creators' id='userAns4'> Answer 4" + "<input type='text' name='ans4' size='15' maxlength='30' />" + "</p>"
+        + "<br />" + "<p class='creators' id='newCorrect'> Correct Answer" + "<input type='text' name='ans4' size='15' maxlength='30' />" + "</p>"
+        + "<button id='nextOne' onclick='createNext();'>Create next question</button>"
+        + "<button id='finishQuiz' onclick='finishQuiz();'>Finish your quiz</button>";
 
+};
+
+//this function needs to grab the values from the array and store them in an object which will get pushed into an array
+var createNext = function() {
+    var userQuestion = document.getElementById("newQuestion").value;
+    var answer1 = document.getElementById("userAns1").value;
+    var answer2 = document.getElementById("userAns2").value;
+    var answer3 = document.getElementById("userAns3").value;
+    var answer4 = document.getElementById("userAns4").value;
+    var usersCorrect = document.getElementById("newCorrect").value;
+
+    //pushes the users question into an array of the users questions.
+    userCreatedQuestions.push(new QuestionConstructor(userQuestion, answer1, answer2, answer3, answer4, usersCorrect));
+    createQuestionCounter++;
+    createQuestion();
+};
+
+var finishQuiz = function () {
+    document.getElementById("question").innerHTML = "";
+    document.getElementById("contentWrapper").className = "hide";
+    document.getElementById("takeQuiz").className = "";
+    document.getElementById("createQuiz").className = "";
+    quizzes.push(userCreatedQuestions);
+};
 //function that displays each question
-var displayQuestion = function () {
+var displayQuestion = function (quizNumber) {
     "use strict";
-
-    if (questionCounter === questions.length) {
+    if (quizNumber !== undefined) {
+        numOfQuiz = quizNumber;
+    }
+    if (questionCounter === quizzes[numOfQuiz].length) {
         displayScore();
     }
 
     //displays the correct question
-    document.getElementById("question").innerHTML = questions[questionCounter]['getQuestion']() + "<h6 id='questionAuthor'> By " + questions[questionCounter]["author"] + "</h6>";
+    document.getElementById("question").innerHTML = quizzes[numOfQuiz][questionCounter]['getQuestion']() + "<h6 id='questionAuthor'> By " + quizzes[numOfQuiz][questionCounter]["author"] + "</h6>";
     // for loop that loops through each question in an object and creates an li element with the question as the innerhtml
     var answerCounter = 0;
-    for (var prop in questions[questionCounter]) {
-        if (questions[questionCounter].hasOwnProperty(prop)) {
+    for (var prop in quizzes[numOfQuiz][questionCounter]) {
+        if (quizzes[numOfQuiz][questionCounter].hasOwnProperty(prop)) {
             if (prop.charAt(3) === "A" && prop.charAt(4) === "n") {
-                document.getElementById("question").innerHTML += "<div id='" + answerCounter + "' class='items' onclick='selectedAnswers[" + questionCounter + "] = " + answerCounter + "; giveBorder(" + answerCounter + " ," + questionCounter + ");'>" + questions[questionCounter]['getAns' + answerCounter]() + "</div>";
+                document.getElementById("question").innerHTML += "<div id='" + answerCounter + "' class='items' onclick='selectedAnswers[" + questionCounter + "] = " + answerCounter + "; giveBorder(" + answerCounter + " ," + questionCounter + ");'>" + quizzes[numOfQuiz][questionCounter]['getAns' + answerCounter]() + "</div>";
                 answerCounter++;
             }
         }
@@ -140,7 +211,7 @@ var displayQuestion = function () {
     document.getElementById("question").innerHTML += "<button id='nextButton' onclick='displayQuestion();' >Next</button>";
 
     //changes "next" button to display "see your score" when user has reached last question
-    if (questionCounter === questions.length - 1) {
+    if (questionCounter === quizzes[numOfQuiz].length - 1) {
         document.getElementById("nextButton").innerHTML = "See your score!";
         document.getElementById("nextButton").setAttribute("onclick", "displayScore()");
     }
@@ -153,7 +224,7 @@ var displayQuestion = function () {
 };
 
 
-
+// resets questionCounter so quiz can begin again
 var backOne = function () {
     questionCounter = questionCounter - 2;
 };
@@ -209,3 +280,4 @@ var displayScore = function () {
 
 };
 
+//alert(quizzes[0].length);
